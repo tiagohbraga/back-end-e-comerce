c12a951f24852bcf61922dd8672ee3c2547541bc
Tiago Braga <tiagohbraga@gmail.com>
2023-01-11 17:08:48 -0300

    exercicios typescript II (deu certo)

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,16 @@
-import { TUsers, TProduct, TPurchase } from "./types"
+import { TUsers, TProduct, TPurchase, PRODUCT_CATEGORY } from "./types"
+
 
 export const users: TUsers[] = [
     {
         id: "a001",
-        email : "tiagohbraga@gmail.com", 
-        password : "entrei"
+        email: "tiagohbraga@gmail.com",
+        password: "entrei"
     },
     {
         id: "a002",
-        email : "ludmila@gmail.com", 
-        password : "oi"
+        email: "ludmila@gmail.com",
+        password: "oi"
     }
 ]
 
@@ -20,14 +21,14 @@ export const product: TProduct[] = [
         id: "p001",
         name: "Banana",
         price: 5,
-        category: "Fruta"
+        category: PRODUCT_CATEGORY.FRUITS
     },
 
     {
         id: "p002",
         name: "Maça",
         price: 5,
-        category: "Fruta"
+        category: PRODUCT_CATEGORY.FRUITS
     }
 
 ]
@@ -43,10 +44,69 @@ export const purchase: TPurchase[] = [
     },
 
     {
-        userId : "a002",
-        productId : "p002",
-        quantity : 3,
+        userId: "a002",
+        productId: "p002",
+        quantity: 3,
         totalPrice: 15
     }
 
 ]
+
+const createUser = (id: string, email: string, password: string) => {
+    let newUser = {
+        id: id,
+        email: email,
+        password: password
+    }
+    return users.push(newUser)
+}
+export const getAllUsers = ()=>{
+    return console.table(users)
+}
+createUser("a003", "fula@gmail.com", "fulano123")
+
+const createProduct = (id : string, name : string, price : number, category : PRODUCT_CATEGORY) => {
+    let newProduct ={
+        id:id,
+        name:name,
+        price:price,
+        category:category
+    }
+    return product.push(newProduct)
+}
+export const getAllProducts = () =>{
+    return console.table(product)
+}
+createProduct("p003", "Uva", 5, PRODUCT_CATEGORY.FRUITS)
+
+export const getProductById = (id:string)=>{
+    let idToSearch = product.filter((product)=>{
+        return product.id === id
+    })
+    return console.table(idToSearch)
+}
+
+export const queryProductsByName = (name:string)=>{
+    let nameToSearch = product.filter((product)=>{
+        return product.name === name
+    })
+    return console.table(nameToSearch)
+}
+
+export const createPurchase = (userId : string, productId: string, quantity: number, totalPrice: number)=>{
+    let newPurchase ={
+        userId:userId,
+        productId: productId,
+        quantity: quantity,
+        totalPrice: totalPrice
+    }
+    return purchase.push(newPurchase)
+}
+createPurchase("a003", "p003", 3, 15)
+
+export const getAllPurchasesFromUserId =(userId: string)=>{
+    let userIdToSearch = purchase.filter ((purchase)=>{
+        return purchase.userId === userId
+    })
+    return console.table(userIdToSearch)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import {users, product, purchase} from "./database"
+import {users, 
+    product, 
+    purchase, 
+    getAllUsers, 
+    getAllProducts, 
+    getProductById, 
+    queryProductsByName,
+    createPurchase,
+    getAllPurchasesFromUserId
+} from "./database"
 
 console.log(users)
 console.log(product)
 console.log(purchase)
-
+getAllUsers()
+getAllProducts()
+getProductById("p001")
+queryProductsByName("Uva")
+createPurchase
+getAllPurchasesFromUserId("a002")
 
 // console.log("Olá")
